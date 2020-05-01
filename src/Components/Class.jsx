1b4b00296f42e7c9c52df88b957def07de5b17ea
Ali Sadoughi <asadoughi1990@gmail.com>
2020-05-01 22:15:17 +0430

    initial content added part-1

--- src/Components/Class.jsx
+++ src/Components/Class.jsx
@@ -19,37 +19,40 @@ function Class() {
           <span>مقطع دبستان</span>
         </div>
         <div className="container">
-          <div className="row">
-            <div className="col mb-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">هندسه</p>
-                </div>
+          <div className="card-deck mb-3">
+            <div className="card">
+              <img
+                src="assets/img/dabestan1.jpeg"
+                className="card-img-top"
+                alt="dab1"
+              />
+              <div className="card-body">
+                <p className="card-text">
+                  <a href="https://www.aparat.com/v/jXwO4">
+                    آموزش مساحت متوازی الاضلاع
+                  </a>
+                </p>
               </div>
             </div>
-            <div className="col mb-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">ریاضی</p>
-                </div>
+
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">ریاضی</p>
               </div>
             </div>
-            <div className="col mb-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">جغرافیا</p>
-                </div>
+
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">جغرافیا</p>
               </div>
             </div>
-            <div className="col mb-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">گسسته</p>
-                </div>
+
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">تاریخ</p>
               </div>
             </div>
           </div>
@@ -59,37 +62,62 @@ function Class() {
           <span>مقطع متوسطه اول</span>
         </div>
         <div className="container">
-          <div className="row">
-            <div className="col mb-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">هندسه</p>
-                </div>
+          <div className="card-deck mb-3">
+            <div className="card">
+              <img
+                src="assets/img/mo1-1.jpeg"
+                className="card-img-top"
+                alt="mo1-1"
+              />
+              <div className="card-body">
+                <p className="card-text">
+                  <a href="https://www.aparat.com/v/r45uC">
+                    آینه های محدب و مقعر و تخت
+                  </a>
+                </p>
               </div>
             </div>
-            <div className="col mb-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">ریاضی</p>
-                </div>
+
+            <div className="card">
+              <img
+                src="assets/img/mo1-2.jpeg"
+                className="card-img-top"
+                alt="mo1-2"
+              />
+              <div className="card-body">
+                <p className="card-text">
+                  <a href="https://www.aparat.com/v/htoP0">
+                    زیست هفتم - دستگاه گرد مواد
+                  </a>
+                </p>
               </div>
             </div>
-            <div className="col mb-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">جغرافیا</p>
-                </div>
+
+            <div className="card">
+              <img
+                src="assets/img/mo1-3.jpeg"
+                className="card-img-top"
+                alt="mo1-3"
+              />
+              <div className="card-body">
+                <p className="card-text">
+                  <a href="https://www.aparat.com/v/0WO29">ریاضی نهم فصل ششم</a>
+                </p>
               </div>
             </div>
-            <div className="col mb-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">گسسته</p>
-                </div>
+
+            <div className="card">
+              <img
+                src="assets/img/mo1-4.jpeg"
+                className="card-img-top"
+                alt="mo1-4"
+              />
+              <div className="card-body">
+                <p className="card-text">
+                  <a href="https://www.aparat.com/v/SIAy1?playlist=306722">
+                    علوم هشتم مبحث نور و بازتاب نور
+                  </a>
+                </p>
               </div>
             </div>
           </div>
@@ -99,37 +127,62 @@ function Class() {
           <span>مقطع متوسطه دوم</span>
         </div>
         <div className="container">
-          <div className="row">
-            <div className="col mb-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">هندسه</p>
-                </div>
+          <div className="card-deck mb-3">
+            <div className="card">
+              <img
+                src="assets/img/mo2-1.jpeg"
+                className="card-img-top"
+                alt="mo2-1"
+              />
+              <div className="card-body">
+                <p className="card-text">
+                  <a href="https://www.aparat.com/v/sCHGy">شیمی دهم فصل سوم</a>
+                </p>
               </div>
             </div>
-            <div className="col mb-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">ریاضی</p>
-                </div>
+
+            <div className="card">
+              <img
+                src="assets/img/mo2-2.jpeg"
+                className="card-img-top"
+                alt="mo2-2"
+              />
+              <div className="card-body">
+                <p className="card-text">
+                  <a href="https://www.aparat.com/v/ESj2V">
+                    عربی : قواعد درس پنجم و ششم پایه یازدهم
+                  </a>
+                </p>
               </div>
             </div>
-            <div className="col mb-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">جغرافیا</p>
-                </div>
+
+            <div className="card">
+              <img
+                src="assets/img/mo2-3.jpeg"
+                className="card-img-top"
+                alt="mo2-3"
+              />
+              <div className="card-body">
+                <p className="card-text">
+                  <a href="https://www.aparat.com/v/dLqxz">
+                    هندسه دوازدهم فصل سوم
+                  </a>
+                </p>
               </div>
             </div>
-            <div className="col mb-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">گسسته</p>
-                </div>
+
+            <div className="card">
+              <img
+                src="assets/img/mo2-4.jpeg"
+                className="card-img-top"
+                alt="mo2-4"
+              />
+              <div className="card-body">
+                <p className="card-text">
+                  <a href="https://www.aparat.com/v/AOkBp">
+                    حسابان مبحث تابع نمایی و لگاریتمی
+                  </a>
+                </p>
               </div>
             </div>
           </div>
--- src/Components/Learn.jsx
+++ src/Components/Learn.jsx
@@ -8,25 +8,24 @@ function Learning() {
           آموزش استفاده از سامانه آموزش آنلاین
         </div>
         <div className="container">
-          <div className="row">
-            <div className="col mb-3">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">
-                    1- ورود به سامانه کاربران
-                  </p>
-                </div>
+          <div class="card-deck mb-4">
+            <div className="card">
+              <img
+                src="assets/img/learn1.jpeg"
+                className="card-img-top"
+                alt="learn1"
+              />
+              <div className="card-body">
+                <p className="card-text">1- ورود به سامانه کاربران</p>
               </div>
             </div>
-            <div className="col mb-3">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">
-                    2- استفاده از اپلیکیشن تحت وب اسکای روم
-                  </p>
-                </div>
+
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">
+                  2- استفاده از اپلیکیشن تحت وب اسکای روم
+                </p>
               </div>
             </div>
           </div>
