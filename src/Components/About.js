0d7bcb3eaa38884c66afc700c2ce1c6a34aab9c6
Ali Sadoughi <asadoughi1990@gmail.com>
2021-01-28 13:21:38 +0330

    changes in about and video sections

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,19 +1,33 @@
 import React from "react";
 
+import Button from "react-bootstrap/Button";
+
 const about = () => {
   return (
     <section id="about">
       <div className="container">
-        <div className="about-text mb-3">
-          چشم انداز مجتمع فرهنگی آموزشی دکتر هشتـرودی تربیت شهروندان توانمند، یعنی
-          ایرانیانی سالم از نظر جسمی و روحی، با ایمان، آگاه به تاریخ افتخار آمیز
-          فرهنگ و تمـدّن سرشار از حکمت و عرفان ایـران، فـرهیخته و با شـخصیت،
-          آگاه بــه علوم و فـن‌آوری روز، هـنرمند و ماهر در زندگی، با اعتماد به
-          نفس و سازنده ایران فردا، با این باور که علم منبع اصلی تـولـیـد ثـروت و
-          کسب قدرت اســت و هنر و اخلاق راهگشای سعادت بشر.
+        <div className="about-text">
+          چشم انداز مجتمع فرهنگی آموزشی دکتر هشتـرودی تربیت شهروندان توانمند،
+          یعنی ایرانیانی سالم از نظر جسمی و روحی، با ایمان، آگاه به تاریخ افتخار
+          آمیز فرهنگ و تمـدّن سرشار از حکمت و عرفان ایـران، فـرهیخته و با
+          شـخصیت، آگاه بــه علوم و فـن‌آوری روز، هـنرمند و ماهر در زندگی، با
+          اعتماد به نفس و سازنده ایران فردا، با این باور که علم منبع اصلی
+          تـولـیـد ثـروت و کسب قدرت اســت و هنر و اخلاق راهگشای سعادت بشر.
         </div>
+        <Button
+          className="mb-4"
+          variant="light"
+          href="http://drhashtroudi.ir/fa/page/view/1177/%D8%AF%D8%B1%D8%A8%D8%A7%D8%B1%D9%87-%D9%85%D8%A7"
+          target="_blank"
+        >
+          توضیحات بیشتر
+        </Button>
       </div>
-      <img src="assets/img/madrese.gif" className="img-fluid about-gif" alt="about" />
+      <img
+        src="assets/img/madrese.gif"
+        className="img-fluid about-gif"
+        alt="about"
+      />
     </section>
   );
 };
--- src/Components/Video.js
+++ src/Components/Video.js
@@ -3,7 +3,39 @@ import React from "react";
 const video = () => {
   return (
     <section id="video">
-      <div className="video-header">معرفی اجمالی مجتمع دکتر هشترودی</div>
+      <div className="video-header">گردش مجازی در مجتمع</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <iframe
+              title="بازدید مجازی از دبستان"
+              src="https://www.aparat.com/video/video/embed/videohash/qKv3s/vt/frame"
+              allowFullScreen={true}
+              webkitallowfullscreen="true"
+              mozallowfullscreen="true"
+            ></iframe>
+          </div>
+          <div className="col-md-4">
+            <iframe
+              title="آغاز متفاوت سال تحصیلی در مجتمع"
+              src="https://www.aparat.com/video/video/embed/videohash/sb341/vt/frame"
+              allowFullScreen={true}
+              webkitallowfullscreen="true"
+              mozallowfullscreen="true"
+            ></iframe>
+          </div>
+          <div className="col-md-4">
+            <iframe
+              title="نگاهی مختصر به فعالیت های آنلاین مجتمع"
+              src="https://www.aparat.com/video/video/embed/videohash/3LQJv/vt/frame"
+              allowFullScreen={true}
+              webkitallowfullscreen="true"
+              mozallowfullscreen="true"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+      <div className="video-header mt-5">فعالیت های پژوهشی و جشنواره ها</div>
       <div className="container">
         <div className="row">
           <div className="col-md-4">
